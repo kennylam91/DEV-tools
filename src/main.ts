bbe39c898bb2542fa9b81a7c59645a9292548dd9
lampv <lam.phamvan@nttdata.com>
2024-06-27 11:53:52 +0700

    add primevue3

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import './assets/main.css'
-
+import '/node_modules/primeflex/primeflex.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/lara-light-indigo/theme.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
