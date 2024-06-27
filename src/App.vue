6f2e4698cb398af9092440d0dbaadf8d6c88886f
lampv <lam.phamvan@nttdata.com>
2024-06-27 14:04:46 +0700

    add log work button

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 
 defineProps<{
-  msg: string
 }>()
-
-const str = ref('')
 
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
