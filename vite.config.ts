50e8976e7cf8faa98f88447c76d6b14325c9e350
lampv <lam.phamvan@nttdata.com>
2024-06-27 11:53:52 +0700

    add primevue3

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import VueRouter from 'unplugin-vue-router/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
+import { PrimeVueResolver } from '@primevue/auto-import-resolver'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -29,6 +30,9 @@ export default defineConfig({
         filepath: './.eslintrc-auto-import.json',
         globalsPropValue: true
       }
+    }),
+    Components({
+      resolvers: [PrimeVueResolver()]
     })
   ],
   resolve: {
