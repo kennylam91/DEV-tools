b9a27bc763d8b6aa7536b51679a571f4863c7d85
Lam Pham Van <lam.phamvan@emeal.nttdata.com>
2025-04-02 10:09:11 +0700

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import 'vue3-json-viewer/dist/index.css'
 import './assets/main.css'
+// import 'highlight.js/styles/monokai.css'
 import '/node_modules/primeflex/primeflex.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
 import JsonViewer from 'vue3-json-viewer'
+import Markdown from 'vue3-markdown-it'
 
 import 'primevue/resources/themes/aura-light-indigo/theme.css'
 
@@ -16,5 +18,6 @@ const app = createApp(App)
 app.use(router)
 app.use(PrimeVue)
 app.use(JsonViewer)
+app.use(Markdown)
 
 app.mount('#app')
--- src/views/lunch-suggestion-ai/index.vue
+++ src/views/lunch-suggestion-ai/index.vue
@@ -1,36 +1,81 @@
 <script setup lang="ts">
 import { chatWithGenAI } from '@/services/genai-service'
+import Markdown from 'vue3-markdown-it'
 
 const userInput = ref()
-const response = ref()
+const responses = ref<{ content: string; fromUser?: boolean }[]>([])
 
+const isWaitingResponse = ref(false)
 const makeRequest = () => {
-  chatWithGenAI(userInput.value).then((res) => {
-    if (res.data?.choices) {
-      response.value = res.data.choices[0].message.content
-    }
-  })
+  if (isWaitingResponse.value) {
+    return
+  }
+  responses.value.push({ content: userInput.value, fromUser: true })
+  userInput.value = ''
+  isWaitingResponse.value = true
+  chatWithGenAI(userInput.value)
+    .then((res) => {
+      if (res.data?.choices) {
+        responses.value.push({ content: res.data.choices[0].message.content })
+        chatInputRef.value.$el.focus()
+      }
+    })
+    .finally(() => {
+      isWaitingResponse.value = false
+    })
 }
+
+const chatInputRef = ref()
 </script>
 
 <template>
   <div class="mx-auto" style="max-width: 50rem">
+    <div class="chatWrapper">
+      <Markdown
+        :class="['message', item.fromUser && 'from-user']"
+        v-for="(item, index) in responses"
+        :key="index"
+        :source="item.content"
+      ></Markdown>
+      <Markdown
+        v-if="isWaitingResponse"
+        class="message"
+        :source="'Chờ xíu, em gái đang trả lời ...'"
+      />
+    </div>
     <Textarea
+      ref="chatInputRef"
+      autofocus
       v-model="userInput"
       placeholder="Anh trai hôm nay muốn ăn gì nào?"
       @keyup.ctrl.enter="makeRequest"
       class="w-full"
     />
-    <span class="text-xs text-500">Ctrl + Enter để submit nhé</span>
-    <div v-if="response" class="chatWrapper" v-html="response"></div>
+    <span class="text-xs text-500">Ctrl + Enter để gửi nhé</span>
   </div>
 </template>
 
 <style lang="css" scoped>
 .chatWrapper {
-  min-height: 200px;
-  /* border: solid 1px gray; */
+  display: block;
+  min-height: calc(100vh - 300px);
+  max-height: calc(100vh - 300px);
+  overflow-y: auto;
   border-radius: 8px;
   margin-top: 3rem;
+}
+
+.message {
+  border: solid 1px rgb(223, 217, 217);
+  border-radius: 16px;
+  padding: 0 1rem;
+  background-color: white;
+  width: fit-content;
+  max-width: 80%;
+  margin-bottom: 1rem;
+}
+
+.message.from-user {
+  margin-left: auto;
 }
 </style>
